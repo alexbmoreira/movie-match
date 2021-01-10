1a3707097f674824d3f3151d104df0b00d50c3e9
alexbmoreira <alexbmoreira@gmail.com>
2021-01-10 16:19:53 -0500

    Added Friendship model and functions

--- profiles/models.py
+++ profiles/models.py
@@ -1,9 +1,28 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
+from django.db.models import Q
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
+    def get_friends(self):
+        friendship_set = Friendship.objects.filter(accepted=True).filter(Q(creator=self.user)|Q(friend=self.user))
+
+        return [fshp.creator if fshp.friend == self.user else fshp.friend for fshp in friendship_set]
+
+    def get_friend_requests(self):
+        friendship_set = Friendship.objects.filter(accepted=False).filter(Q(creator=self.user)|Q(friend=self.user))
+
+        return [fshp.creator if fshp.friend == self.user else fshp.friend for fshp in friendship_set]
+
     def __str__(self):
         return self.user.username
+
+class Friendship(models.Model):
+    creator = models.ForeignKey(User, related_name='friendship_creator_set', on_delete=models.CASCADE)
+    friend = models.ForeignKey(User, related_name='friendship_friend_set', on_delete=models.CASCADE)
+    accepted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.creator} and {self.friend}"
