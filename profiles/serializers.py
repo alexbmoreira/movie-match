0a323aadc93c444fdfe3e4f0644ca99f8b6b96a3
alexbmoreira <alexbmoreira@gmail.com>
2021-01-10 14:38:08 -0500

    Add profile serializer

---
+++ profiles/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import Profile
+
+class ProfileSerializer(serializers.ModelSerializer):
+
+    user = serializers.StringRelatedField(read_only=True)
+    
+    class Meta:
+        model = Profile
+        fields = ['user']
