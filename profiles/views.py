cccd1c51f1adeb5c05dcf0a0ec1dad64125367dd
alexbmoreira <alexbmoreira@gmail.com>
2021-01-13 16:16:53 -0500

    Removed unnecessary imports

--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,6 @@
-import io
-
 from django.db.models import Q
 from django.shortcuts import get_object_or_404, render
 from rest_framework import status
-from rest_framework.parsers import JSONParser
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
