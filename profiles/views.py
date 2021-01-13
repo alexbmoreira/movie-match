b0a96dd82608e62317373cba33faa107cce5854b
alexbmoreira <alexbmoreira@gmail.com>
2021-01-13 00:29:01 -0500

    Added POST for friending users

--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import get_object_or_404, render
+from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .models import Profile, FriendsList
-from .serializers import ProfileSerializer, FriendsListSerializer
+from .models import FriendsList, Profile, User
+from .serializers import FriendsListSerializer, ProfileSerializer
 
 
 class ProfileAPIView(APIView):
@@ -44,6 +45,16 @@ class ProfileFriendsAPIView(APIView):
         serializer = FriendsListSerializer(friends_list)
 
         return Response(data=serializer.data)
+
+    def post(self, request, user_id):
+        add_to_users_friends = User.objects.get(id=user_id)
+        friends_list = FriendsList.objects.get(user=add_to_users_friends)
+
+        add_user_username = request.data['username']
+        add_user = User.objects.get(username=add_user_username)
+        friends_list.friend(add_user)
+            
+        return Response(status=status.HTTP_201_CREATED)
 
 
 # class ProfileFriendshipsAPIView(APIView):
