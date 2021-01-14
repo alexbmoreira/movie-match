2fc5fefec002f3cecef854cfe13fb933222c751e
alexbmoreira <alexbmoreira@gmail.com>
2021-01-13 19:24:02 -0500

    Added views for movie and people metadata requests

--- movies/views.py
+++ movies/views.py
@@ -3,11 +3,12 @@ from django.conf import settings
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+api_key = settings.TMDB_API
+
 
 class MovieSearchAPIView(APIView):
 
     def get(self, request, search, page=1):
-        api_key = settings.TMDB_API
         query = f"https://api.themoviedb.org/3/search/movie?api_key={api_key}&query={search}&page={page}"
 
         response = requests.get(query)
@@ -18,8 +19,6 @@ class MovieSearchAPIView(APIView):
 class ActorSearchAPIView(APIView):
 
     def get(self, request, search, page=1):
-        api_key = settings.TMDB_API
-
         query = f"https://api.themoviedb.org/3/search/person?api_key={api_key}&query={search}&page={page}"
 
         response = requests.get(query)
@@ -31,11 +30,29 @@ class ActorSearchAPIView(APIView):
 class CrewSearchAPIView(APIView):
 
     def get(self, request, search, page=1):
-        api_key = settings.TMDB_API
-
         query = f"https://api.themoviedb.org/3/search/person?api_key={api_key}&query={search}&page={page}"
 
         response = requests.get(query)
         actors = response.json()
         actors['results'] = [actor for actor in actors['results'] if actor['known_for_department'] != "Acting"]
         return Response(actors)
+
+
+class MovieMetadataAPIView(APIView):
+
+    def get(self, request, movie_id):
+        query = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={api_key}"
+
+        response = requests.get(query)
+        movie = response.json()
+        return Response(movie)
+
+
+class PersonMetadataAPIView(APIView):
+
+    def get(self, request, person_id):
+        query = f"https://api.themoviedb.org/3/person/{person_id}?api_key={api_key}"
+
+        response = requests.get(query)
+        person = response.json()
+        return Response(person)
