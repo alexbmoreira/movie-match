ba6814f01d0e5d05b33928cdf2b0fd1d3b90a63e
alexbmoreira <alexbmoreira@gmail.com>
2021-01-13 20:05:22 -0500

    Added a list of credits a certain person would be known for

--- movies/views.py
+++ movies/views.py
@@ -55,4 +55,16 @@ class PersonMetadataAPIView(APIView):
 
         response = requests.get(query)
         person = response.json()
+        
+        person['credits'] = self.sort_credits(person_id, person['known_for_department'])
         return Response(person)
+
+    def sort_credits(self, person_id, known_for):
+        query = f"https://api.themoviedb.org/3/person/{person_id}/movie_credits?api_key={api_key}"
+        response = requests.get(query)
+        person_credits = response.json()
+        
+        credit_list = "cast" if known_for != "Acting" else "crew"
+        person_credits['known_for_credits'] = sorted(person_credits[credit_list], key=lambda k: k['popularity'], reverse=True)[0:10]
+
+        return person_credits
