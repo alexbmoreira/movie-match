441f47402a5413575f259af402ee7ae260ef39a0
alexbmoreira <alexbmoreira@gmail.com>
2021-01-15 10:40:49 -0500

    Made breakpoints work for mobile first instead

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,22 +2,22 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    screens: {
-      '2xl': { max: '1535px' },
-      // => @media (max-width: 1535px) { ... }
+    // screens: {
+    //   '2xl': { max: '1535px' },
+    //   // => @media (max-width: 1535px) { ... }
 
-      'xl': { max: '1279px' },
-      // => @media (max-width: 1279px) { ... }
+    //   'xl': { max: '1279px' },
+    //   // => @media (max-width: 1279px) { ... }
 
-      'lg': { max: '1023px' },
-      // => @media (max-width: 1023px) { ... }
+    //   'lg': { max: '1023px' },
+    //   // => @media (max-width: 1023px) { ... }
 
-      'md': { max: '767px' },
-      // => @media (max-width: 767px) { ... }
+    //   'md': { max: '767px' },
+    //   // => @media (max-width: 767px) { ... }
 
-      'sm': { max: '639px' }
-      // => @media (max-width: 639px) { ... }
-    },
+    //   'sm': { max: '639px' }
+    //   // => @media (max-width: 639px) { ... }
+    // },
     fontFamily: {
       'nunito': ['Nunito', 'sans-serif'],
       'roboto-slab': ['Roboto Slab', 'serif']
