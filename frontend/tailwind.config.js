c9a8127389b5000e374b4f3a96ebd7076c8c5991
alexbmoreira <alexbmoreira@gmail.com>
2021-01-15 10:43:01 -0500

    Made colors and fonts extend existing ones

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -18,35 +18,37 @@ module.exports = {
     //   'sm': { max: '639px' }
     //   // => @media (max-width: 639px) { ... }
     // },
-    fontFamily: {
-      'nunito': ['Nunito', 'sans-serif'],
-      'roboto-slab': ['Roboto Slab', 'serif']
-    },
-    colors: {
-      'app-bg': {
-        light: '#707070',
-        DEFAULT: '#616161',
-        dark: '#5C5C5C'
+    extend: {
+      fontFamily: {
+        'nunito': ['Nunito', 'sans-serif'],
+        'roboto-slab': ['Roboto Slab', 'serif']
       },
-      'app-bg-sec': {
-        light: '#D6D6D6',
-        DEFAULT: '#CBCBCB',
-        dark: '#C2C2C2'
-      },
-      'app-primary': {
-        dark: '#60548C',
-        DEFAULT: '#534879',
-        light: '#4F4573'
-      },
-      'app-accent': {
-        dark: '#E9CD5D',
-        DEFAULT: '#E5C442',
-        light: '#E4C23A'
-      },
-      'app-typeface': {
-        dark: '#EBEBEB',
-        DEFAULT: '#F0F0F0',
-        light: '#FFFFFF'
+      colors: {
+        'app-bg': {
+          light: '#707070',
+          DEFAULT: '#616161',
+          dark: '#5C5C5C'
+        },
+        'app-bg-sec': {
+          light: '#D6D6D6',
+          DEFAULT: '#CBCBCB',
+          dark: '#C2C2C2'
+        },
+        'app-primary': {
+          dark: '#60548C',
+          DEFAULT: '#534879',
+          light: '#4F4573'
+        },
+        'app-accent': {
+          dark: '#E9CD5D',
+          DEFAULT: '#E5C442',
+          light: '#E4C23A'
+        },
+        'app-typeface': {
+          dark: '#EBEBEB',
+          DEFAULT: '#F0F0F0',
+          light: '#FFFFFF'
+        }
       }
     }
   },
