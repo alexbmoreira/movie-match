030876c23cb77628e9a0f00ad03a28e9a8988916
alexbmoreira <alexbmoreira@gmail.com>
2021-01-23 00:41:24 -0500

    Joint watchlists update on adding and removing from any user's watchlist

--- profiles/signals.py
+++ profiles/signals.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.models import User
+from django.db.models import Q
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from .models import (FriendRequest, FriendsList, JointWatchlist, Profile,
-                     Watchlist, friend_request_accepted, movie_added)
+                     Watchlist, friend_request_accepted, movie_updated)
 
 
 @receiver(post_save, sender=User)
@@ -20,3 +21,11 @@ def create_profile(sender, instance, created, **kwargs):
 def my_receiver(creator, receiver, **kwargs):
     j_w = JointWatchlist.objects.create(user1=creator, user2=receiver)
     j_w.save()
+
+
+@receiver(movie_updated, sender=Watchlist)
+def my_receiver(user, **kwargs):
+    watchlists = JointWatchlist.objects.filter(Q(user1=user) | Q(user2=user))
+    for j_w in watchlists:
+        j_w.save()
+
