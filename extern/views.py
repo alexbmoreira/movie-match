df17d1dd1660ae6b9884763b894bd4980972ecb9
alexbmoreira <alexbmoreira@gmail.com>
2021-02-05 10:31:10 -0500

    User can request a watchlist from letterboxd and add to their list

--- extern/views.py
+++ extern/views.py
@@ -5,14 +5,21 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .utils import Letterboxd
+from profiles.models import Watchlist
 
 
 class LetterboxdWatchlistAPIView(APIView):
 
-    def get(self, request, username):
+    def post(self, request, username):
         watchlist = Letterboxd.get_watchlist(username)
 
         if "error" not in watchlist:
-            return Response(watchlist)
+            user_list = Watchlist.objects.get(user=request.user)
+            for movie in watchlist:
+                if movie["id"] not in user_list.watchlist:
+                    user_list.add_movie(movie["id"])
+            
+            user_list.save()
+            return Response(status=status.HTTP_202_ACCEPTED)
         else:
             return Response(watchlist, status=status.HTTP_404_NOT_FOUND)
