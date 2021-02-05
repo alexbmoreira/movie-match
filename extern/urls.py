d2c8c3a8051f079d32f657e1a33179f0ea8ce89d
alexbmoreira <alexbmoreira@gmail.com>
2021-02-05 10:42:36 -0500

    User can append or overwrite watchlists

--- extern/urls.py
+++ extern/urls.py
@@ -6,5 +6,6 @@ lbxd_wlist_api = LetterboxdWatchlistAPIView.as_view()
 
 
 urlpatterns = [
-    path("letterboxd/watchlist/<str:username>/", lbxd_wlist_api, name="letterboxd_watchlist")
+    path("letterboxd/watchlist/<str:username>/", lbxd_wlist_api, name="letterboxd_watchlist"),
+    path("letterboxd/watchlist/<str:username>/<str:operation>/", lbxd_wlist_api, name="letterboxd_watchlist_action")
 ]
--- extern/views.py
+++ extern/views.py
@@ -10,15 +10,19 @@ from profiles.models import Watchlist
 
 class LetterboxdWatchlistAPIView(APIView):
 
-    def post(self, request, username):
+    def post(self, request, username, operation="add"):
         watchlist = Letterboxd.get_watchlist(username)
 
         if "error" not in watchlist:
             user_list = Watchlist.objects.get(user=request.user)
+
+            if operation == "overwrite":
+                user_list.watchlist.clear()
+
             for movie in watchlist:
                 if movie["id"] not in user_list.watchlist:
                     user_list.add_movie(movie["id"])
-            
+
             user_list.save()
             return Response(status=status.HTTP_202_ACCEPTED)
         else:
