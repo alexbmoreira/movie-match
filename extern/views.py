3bd7b8c2c556c2f46df885de513e70dce13c6072
alexbmoreira <alexbmoreira@gmail.com>
2021-02-05 01:24:30 -0500

    Created view for getting user watchlist

--- extern/views.py
+++ extern/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+import requests
+from django.conf import settings
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from utils import Letterboxd
+
+
+class LetterboxdWatchlistAPIView(APIView):
+
+    def get(self, request, username):
+        watchlist = Letterboxd.get_watchlist(username)
+
+        return Response(watchlist)
