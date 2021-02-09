3fe76d60850857d3ea4ee78c84ace120f6881741
alexbmoreira <alexbmoreira@gmail.com>
2021-02-09 16:26:25 -0500

    Redid transistions

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -17,35 +17,53 @@
       </div>
 
       <!-- Search field -->
-      <div v-if="show_search" class="flex h-0 space-x-3">
-        <input
-          v-model="search.string"
-          type="text"
-          placeholder="Search for a movie..."
-          class="bg-transparent w-full px-3 pb-2 my-auto border-b-2 border-app-bg-sec-light focus:border-app-primary transition duration-500 focus:outline-none"
-          @keyup.enter="routeSearch"
-        />
-        <button class="w-auto flex justify-end items-center transition duration-400 ease-in-out hover:text-app-primary" @click.prevent="routeSearch"><i class="fas fa-search"></i></button>
-        <div class="flex flex-col">
-          <label for="searchType" class="text-xs">Search for:</label>
-          <select id="searchType" v-model="search.type" class="bg-app-bg rounded">
-            <option value="movies">Movies</option>
-            <option value="actors">Actors</option>
-            <option value="crew">Crew</option>
-          </select>
+      <transition
+        enter-active-class="transistion-all duration-100 ease-in"
+        leave-active-class="transistion-all duration-100 ease-out"
+        enter-class="opacity-0 h-0"
+        enter-to-class="opacity-100 h-12"
+        leave-class="opacity-100 h-12"
+        leave-to-class="opacity-0 h-0"
+      >
+        <div v-if="show_search" class="flex space-x-3">
+          <input
+            v-model="search.string"
+            type="text"
+            placeholder="Search for a movie..."
+            class="bg-transparent w-full px-3 pb-2 my-auto border-b-2 border-app-bg-sec-light focus:border-app-primary transition duration-500 focus:outline-none"
+            @keyup.enter="routeSearch"
+          />
+          <button class="w-auto flex justify-end items-center transition duration-400 ease-in-out hover:text-app-primary" @click.prevent="routeSearch"><i class="fas fa-search"></i></button>
+          <div class="flex flex-col">
+            <label for="searchType" class="text-xs">Search for:</label>
+            <select id="searchType" v-model="search.type" class="bg-app-bg rounded">
+              <option value="movies">Movies</option>
+              <option value="actors">Actors</option>
+              <option value="crew">Crew</option>
+            </select>
+          </div>
         </div>
-      </div>
+      </transition>
 
       <!-- Logins -->
-      <div v-if="show_menu" class="flex h-0">
-        <div v-show="!isLoggedIn" class="flex my-auto mx-auto space-x-4">
-          <button class="bg-transparent rounded border border-app-typeface px-2 py-1 transition duration-400 ease-in-out hover:bg-app-primary hover:border-opacity-0" @click.prevent="goLogin()">Log In</button>
-          <button class="bg-transparent px-2 py-1 transition duration-400 ease-in-out hover:text-app-primary" @click.prevent="goRegister()">Register</button>
+      <transition
+        enter-active-class="transistion-all duration-100 ease-in"
+        leave-active-class="transistion-all duration-100 ease-out"
+        enter-class="opacity-0 h-0"
+        enter-to-class="opacity-100 h-12"
+        leave-class="opacity-100 h-12"
+        leave-to-class="opacity-0 h-0"
+      >
+        <div v-if="show_menu" class="flex">
+          <div v-show="!isLoggedIn" class="flex my-auto mx-auto space-x-4">
+            <button class="bg-transparent rounded border border-app-typeface px-2 py-1 transition duration-400 ease-in-out hover:bg-app-primary hover:border-opacity-0" @click.prevent="goLogin()">Log In</button>
+            <button class="bg-transparent px-2 py-1 transition duration-400 ease-in-out hover:text-app-primary" @click.prevent="goRegister()">Register</button>
+          </div>
+          <div v-show="isLoggedIn" class="flex my-auto mx-auto space-x-4">
+            <button class="bg-transparent px-2 py-1 transition duration-400 ease-in-out hover:text-app-primary" @click.prevent="logout()">Log Out</button>
+          </div>
         </div>
-        <div v-show="isLoggedIn" class="flex my-auto mx-auto space-x-4">
-          <button class="bg-transparent px-2 py-1 transition duration-400 ease-in-out hover:text-app-primary" @click.prevent="logout()">Log Out</button>
-        </div>
-      </div>
+      </transition>
     </div>
   </div>
 </template>
