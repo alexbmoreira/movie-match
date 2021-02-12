e5f8510412c1f50aaa26894895d26b238bde2d61
alexbmoreira <alexbmoreira@gmail.com>
2021-02-12 18:23:10 -0500

    Added missing poster image

--- frontend/src/views/Search.vue
+++ frontend/src/views/Search.vue
@@ -7,11 +7,10 @@
     <!-- Movies as search category -->
     <div v-if="search_type === 'movies'" class="divide-y border-b divide-app-bg-light border-app-bg-light">
       <div v-for="result in searchData.results" :key="result.id" class="py-2 flex w-full">
-        <img v-if="result.poster_link_sm !== 'https://image.tmdb.org/t/p/w154None'" class="h-40 md:h-56 object-left border-2 rounded md:my-auto" :src="result.poster_link_md" alt="" />
-        <img v-if="result.poster_link_sm === 'https://image.tmdb.org/t/p/w154None'" class="h-40 md:h-56 object-left border-2 rounded" :src="missing_poster" alt="" />
+        <img class="h-40 md:h-56 object-left border-2 rounded md:my-auto" :src="getPoster(result)" alt="" />
         <div class="flex flex-col mx-2 md:mx-10 md:my-auto">
           <div class="flex flex-wrap">
-            <p class="text-thin text-xl font-roboto-slab">{{ result.title }}</p>
+            <p class="text-thin text-xl font-roboto-slab">{{ getTitle(result) }}</p>
           </div>
           <div>
             <p class="text-thin">{{ result.release_year }}</p>
@@ -36,10 +35,11 @@
     <!-- Actors as search category -->
     <div v-if="search_type === 'actors'" class="divide-y border-b divide-app-bg-light border-app-bg-light">
       <div v-for="result in searchData.results" :key="result.id" class="py-2 flex">
-        <img v-if="result.profile_link_sm !== 'https://image.tmdb.org/t/p/w154None'" class="object-scale-down h-40 md:h-56 object-left border-2 rounded" :src="result.profile_link_sm" alt="" />
-        <img v-if="result.profile_link_sm === 'https://image.tmdb.org/t/p/w154None'" class="object-scale-down h-40 md:h-56 object-left border-2 rounded" :src="missing_poster" alt="" />
+        <img class="h-40 md:h-56 object-left border-2 rounded md:my-auto" :src="getPoster(result)" alt="" />
         <div class="flex flex-col mx-2 md:mx-10 md:my-auto">
-          <p class="text-thin text-xl font-roboto-slab">{{ result.name }}</p>
+          <div class="flex flex-wrap">
+            <p class="text-thin text-xl font-roboto-slab">{{ getTitle(result) }}</p>
+          </div>
           <!--p class="text-thin my-1">{{ result.known_for[0].title }}</p-->
           <p class="mb-6 italic text-bg-app-accent">{{ result.known_for_department }}</p>
           <p class="text-thin text-sm">Known for:</p>
@@ -86,6 +86,20 @@ export default {
     this.makeSearch()
   },
   methods: {
+    getPoster(result) {
+      if (this.search_type == 'movies') {
+        return result.poster_path ? result.poster_link_md : require('@/assets/images/missing_poster.png')
+      } else {
+        return result.profile_path ? result.profile_link_md : require('@/assets/images/missing_poster.png')
+      }
+    },
+    getTitle(result) {
+      if (this.search_type == 'movies') {
+        return result.title
+      } else {
+        return result.name
+      }
+    },
     async makeSearch() {
       if (this.search && this.search.length > 0 && this.search_type.length > 0) {
         this.searchData = await searchAPI.searchMovie(this.search_type, this.search)
