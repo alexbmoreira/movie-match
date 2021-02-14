c138cd184cd1b8101c18db31bfc00da15cf1491b
alexbmoreira <alexbmoreira@gmail.com>
2021-02-13 21:29:30 -0500

    Moved search bar and user buttons to components

---
+++ frontend/src/components/navbarElements/SearchBar.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="flex space-x-3 w-full">
+    <form class="flex w-full" @submit.prevent="routeSearch"><TextField v-model="search.string" :placeholder="`Search for ${search.type}...`" /></form>
+    <button class="w-auto flex justify-end items-center transition duration-400 ease-in-out hover:text-app-primary" @click.prevent="routeSearch">
+      <i class="fas fa-search"></i>
+    </button>
+    <div class="flex flex-col">
+      <label for="searchType" class="text-xs">Search for:</label>
+      <select id="searchType" v-model="search.type" class="bg-app-bg rounded">
+        <option value="movies">Movies</option>
+        <option value="actors">Actors</option>
+        <option value="crew">Crew</option>
+      </select>
+    </div>
+  </div>
+</template>
+
+<script>
+import TextField from '@/components/actions/TextField'
+
+export default {
+  name: 'SearchBar',
+  components: {
+    TextField
+  },
+  data() {
+    return {
+      search: {
+        string: '',
+        type: 'movies'
+      }
+    }
+  },
+  methods: {
+    routeSearch() {
+      if (this.search.string.length > 0) {
+        this.$store.dispatch('makeSearch', this.search)
+        this.$router.push({ name: 'Search' }).catch(() => {})
+      }
+    }
+  }
+}
+</script>
---
+++ frontend/src/components/navbarElements/UserActions.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="flex">
+    <div v-show="!isLoggedIn" class="flex my-auto mx-auto space-x-4">
+      <button class="bg-transparent rounded border border-app-typeface w-20 px-4 py-1 transition duration-400 ease-in-out hover:bg-app-primary hover:border-opacity-0" @click.prevent="goLogin()">Log In</button>
+      <button class="bg-transparent px-2 py-1 transition duration-400 ease-in-out hover:text-app-primary" @click.prevent="goRegister()">Register</button>
+    </div>
+    <div v-show="isLoggedIn" class="flex my-auto mx-auto space-x-4">
+      <button class="bg-transparent px-2 py-1 transition duration-400 ease-in-out hover:text-app-primary" @click.prevent="logout()">Log Out</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserActions',
+  computed: {
+    isLoggedIn: function() {
+      return this.$store.getters.isLoggedIn
+    }
+  },
+  methods: {
+    goLogin() {
+      this.$router.push({ name: 'Login' }).catch(() => {})
+    },
+    goRegister() {
+      this.$router.push({ name: 'Register' }).catch(() => {})
+    },
+    async logout() {
+      try {
+        await this.$store.dispatch('logoutUser')
+        this.$router.push({ name: 'Login' })
+      } catch (err) {
+        console.error(err)
+      }
+    }
+  }
+}
+</script>
