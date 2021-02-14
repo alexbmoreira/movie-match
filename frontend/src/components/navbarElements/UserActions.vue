140da0269348c7a3641e49ceb89f80512755eb04
alexbmoreira <alexbmoreira@gmail.com>
2021-02-13 22:09:53 -0500

    Added profile link to navbar

--- frontend/src/components/navbarElements/UserActions.vue
+++ frontend/src/components/navbarElements/UserActions.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="flex">
-    <div v-show="!isLoggedIn" class="flex my-auto mx-auto space-x-4">
+    <div v-if="!isLoggedIn" class="flex my-auto mx-auto space-x-4">
       <button class="bg-transparent rounded border border-app-typeface w-20 px-4 py-1 transition duration-400 ease-in-out hover:bg-app-primary hover:border-opacity-0" @click.prevent="goLogin()">Log In</button>
-      <button class="bg-transparent px-2 py-1 transition duration-400 ease-in-out hover:text-app-primary" @click.prevent="goRegister()">Register</button>
+      <button class="bg-transparent w-20 px-1 py-1 transition duration-400 ease-in-out hover:text-app-primary" @click.prevent="goRegister()">Register</button>
     </div>
-    <div v-show="isLoggedIn" class="flex my-auto mx-auto space-x-4">
-      <button class="bg-transparent px-2 py-1 transition duration-400 ease-in-out hover:text-app-primary" @click.prevent="logout()">Log Out</button>
+    <div v-else class="flex my-auto mx-auto space-x-1">
+      <div class="flex rounded-full bg-app-bg-sec w-8 h-8">
+        <p class="mx-auto my-auto uppercase">{{ userInitial }}</p>
+      </div>
+      <button class="bg-transparent w-24 px-1 py-1 transition duration-400 ease-in-out hover:text-app-primary" @click.prevent="logout()">Log Out</button>
     </div>
   </div>
 </template>
@@ -13,10 +16,21 @@
 <script>
 export default {
   name: 'UserActions',
+  data() {
+    return {
+      user: {}
+    }
+  },
   computed: {
     isLoggedIn: function() {
       return this.$store.getters.isLoggedIn
+    },
+    userInitial: function() {
+      return this.user.username.substring(0, 1)
     }
+  },
+  created() {
+    this.user = this.$store.getters.user
   },
   methods: {
     goLogin() {
@@ -29,8 +43,8 @@ export default {
     },
     async logout() {
       try {
-        await this.$store.dispatch('logoutUser')
         this.$emit('route')
+        await this.$store.dispatch('logoutUser')
         this.$router.push({ name: 'Login' })
       } catch (err) {
         console.error(err)
