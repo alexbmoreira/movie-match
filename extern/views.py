aefbb5448e69347fa94d867c06e7399a2b9d3f8b
alexbmoreira <alexbmoreira@gmail.com>
2021-04-14 18:04:01 -0400

    Removed all old models and replaced them with basic new ones

--- extern/views.py
+++ extern/views.py
@@ -5,25 +5,25 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .utils import Letterboxd
-from profiles.models import Watchlist
+# from profiles.models import Watchlist
 
 
-class LetterboxdWatchlistAPIView(APIView):
+# class LetterboxdWatchlistAPIView(APIView):
 
-    def post(self, request, username, operation="add"):
-        watchlist = Letterboxd.get_watchlist(username)
+#     def post(self, request, username, operation="add"):
+#         watchlist = Letterboxd.get_watchlist(username)
 
-        if "error" not in watchlist:
-            user_list = Watchlist.objects.get(user=request.user)
+#         if "error" not in watchlist:
+#             user_list = Watchlist.objects.get(user=request.user)
 
-            if operation == "overwrite":
-                user_list.watchlist.clear()
+#             if operation == "overwrite":
+#                 user_list.watchlist.clear()
 
-            for movie in watchlist:
-                if movie["id"] not in user_list.watchlist:
-                    user_list.add_movie(movie["id"])
+#             for movie in watchlist:
+#                 if movie["id"] not in user_list.watchlist:
+#                     user_list.add_movie(movie["id"])
 
-            user_list.save()
-            return Response(status=status.HTTP_202_ACCEPTED)
-        else:
-            return Response(watchlist, status=status.HTTP_404_NOT_FOUND)
+#             user_list.save()
+#             return Response(status=status.HTTP_202_ACCEPTED)
+#         else:
+#             return Response(watchlist, status=status.HTTP_404_NOT_FOUND)
--- profiles/admin.py
+++ profiles/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
-from .models import FriendRequest, FriendsList, Matchlist, Profile, Watchlist
+from .models import (FriendRequest, Friendship, MatchlistDislike,
+                     MatchlistLike, Profile, WatchlistMovie)
 
 admin.site.register(Profile)
-admin.site.register(FriendsList)
+admin.site.register(Friendship)
 admin.site.register(FriendRequest)
-admin.site.register(Watchlist)
-admin.site.register(Matchlist)
+admin.site.register(WatchlistMovie)
+admin.site.register(MatchlistLike)
+admin.site.register(MatchlistDislike)
