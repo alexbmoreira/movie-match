68df3e6e42ccf74f46166005d0d8bd1aae081b93
alexbmoreira <alexbmoreira@gmail.com>
2021-04-16 12:13:31 -0400

    Changed User import

--- profiles/tests/test_models.py
+++ profiles/tests/test_models.py
@@ -1,6 +1,7 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
-from ..models import FriendRequest, Friendship, Profile, User
+from ..models import FriendRequest, Friendship, Profile
 
 
 class FriendRequestTests(TestCase):
