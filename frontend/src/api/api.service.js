12cfcf2ce4be8402ace2f005ce6354dff1ef21b2
alexbmoreira <alexbmoreira@gmail.com>
2021-04-16 23:41:29 -0400

    Added interceptor instead of setting suth every time

--- frontend/src/api/api.service.js
+++ frontend/src/api/api.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import Cookies from 'js-cookie'
 
-export default axios.create({
+const instance = axios.create({
   baseURL: process.env.VUE_APP_URL + 'api',
   mode: 'same-origin',
   headers: {
@@ -9,3 +9,18 @@ export default axios.create({
     'X-CSRFTOKEN': Cookies.get('csrftoken')
   }
 })
+
+instance.interceptors.request.use(
+  async config => {
+    const token = Cookies.get('access_token')
+    if (token) {
+      config.headers.Authorization = `Token ${token}`
+    }
+    return config
+  },
+  err => {
+    return Promise.reject(err)
+  }
+)
+
+export default instance
--- frontend/src/api/friends.js
+++ frontend/src/api/friends.js
@@ -1,21 +1,7 @@
 import api from '@/api/api.service'
-import Cookies from 'js-cookie'
-
-var config = {}
-
-function setAuth() {
-  if (Cookies.get('access_token')) {
-    config = {
-      headers: {
-        Authorization: `Token ${Cookies.get('access_token')}`
-      }
-    }
-  }
-}
 
 async function getFriends(userID) {
-  setAuth()
-  return api.get(`/profiles/${userID}/friends/`, config).then(response => response.data)
+  return api.get(`/profiles/${userID}/friends/`).then(response => response.data)
 }
 
 export default {
