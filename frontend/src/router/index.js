963bf433b69a9dd7897717b25a9dba62840eb2fe
alexbmoreira <alexbmoreira@gmail.com>
2021-04-20 17:14:08 -0400

    User cannot go to login or register pages when they are signed in

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import store from '@/store'
 import Home from '@/views/Home'
 import Profile from '@/views/Profile'
 import Register from '@/views/Register'
@@ -23,12 +24,18 @@ const routes = [
   {
     path: '/register',
     name: 'Register',
-    component: Register
+    component: Register,
+    meta: {
+      requiresNoAuth: true
+    }
   },
   {
     path: '/login',
     name: 'Login',
-    component: Login
+    component: Login,
+    meta: {
+      requiresNoAuth: true
+    }
   },
   {
     path: '/search/:searchType/:search',
@@ -45,6 +52,19 @@ const routes = [
 const router = new VueRouter({
   mode: 'history',
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresNoAuth)) {
+    console.log(store)
+    if (!store.getters.isLoggedIn) {
+      next()
+      return
+    }
+    next('/')
+  } else {
+    next()
+  }
 })
 
 export default router
