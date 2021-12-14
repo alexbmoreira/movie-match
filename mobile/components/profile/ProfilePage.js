6f325f415cca176242d5b8a1cf8bcf7dc54856cd
alexbmoreira <alexbmoreira@gmail.com>
2021-12-13 19:49:36 -0500

    Initial setup

---
+++ mobile/components/profile/ProfilePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+const ProfilePage = () => {
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+}
+
+export default ProfilePage
