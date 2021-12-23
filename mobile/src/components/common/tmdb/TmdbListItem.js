cd8fa016943b5bd7ed92d95eb74ae2b63a443d91
alexbmoreira <alexbmoreira@gmail.com>
2021-12-23 14:49:47 -0500

    prevent click (oops)

--- mobile/src/components/common/tmdb/TmdbListItem.js
+++ mobile/src/components/common/tmdb/TmdbListItem.js
@@ -24,7 +24,7 @@ const style = StyleSheet.create({
 
 const TmdbListItem = ({ header, imageLink, item }) => {
   return (
-    <Pressable onPress={() => navigate('MovieDetails', { movieId: item.id })}>
+    <Pressable onPress={() => item.type === 'movie' && navigate('MovieDetails', { movieId: item.id })}>
       <View style={style.resultItem}>
         <Poster size='sm' title={header} source={{ uri: imageLink }}/>
         <View style={style.info}>
