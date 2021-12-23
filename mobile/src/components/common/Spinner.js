6a1524cf884797567818a2f80168e4a1ea78582c
Alex Moreira <alexbmoreira@gmail.com>
2021-12-22 23:50:18 -0500

    Fix require cycle (#171)

--- mobile/src/components/common/Spinner.js
+++ mobile/src/components/common/Spinner.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
 import { ActivityIndicator } from 'react-native-paper';
-import { theme } from 'shared';
+import theme from 'shared/theme';
 
 const style = StyleSheet.create({
   spinner: {
