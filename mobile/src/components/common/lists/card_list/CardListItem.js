b1f04f1fd8637eb5b2985bafb874820c241d3f40
alexbmoreira <alexbmoreira@gmail.com>
2021-12-23 01:08:01 -0500

    No clipping on card lists

--- mobile/src/components/common/lists/card_list/CardListItem.js
+++ mobile/src/components/common/lists/card_list/CardListItem.js
@@ -1,21 +1,10 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-
-const style = StyleSheet.create({
-  cardListItem: {
-    marginRight: 15
-  }
-});
 
 const CardListItem = ({ model, component }) => {
   if(!component) return null;
 
   const Component = component;
-  return (
-    <View style={style.cardListItem}>
-      <Component model={model}/>
-    </View>
-  );
+  return <Component model={model}/>;
 };
 
 export default CardListItem;
