b10ea7b7b31736cb8296f3257dec3f379e5f372d
Alex Moreira <alexbmoreira@gmail.com>
2022-01-02 18:36:47 -0500

    Make a better icon button with pressable instead (#199)

--- mobile/src/components/common/buttons/IconButton.js
+++ mobile/src/components/common/buttons/IconButton.js
@@ -1,8 +1,23 @@
 import { extendObservable } from 'mobx';
 import { observer } from 'mobx-react';
+import { PropTypes } from 'prop-types';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { IconButton as Btn } from 'react-native-paper';
+import { Pressable, StyleSheet, View } from 'react-native';
+
+const getIconSize = (size) => {
+  switch (size) {
+  case 'xs':
+    return 18;
+  case 'sm':
+    return 24;
+  case 'md':
+    return 30;
+  case 'lg':
+    return 36;
+  default:
+    return 24;
+  }
+};
 
 const defaultStyle = StyleSheet.create({
   iconButton: {
@@ -10,7 +25,7 @@ const defaultStyle = StyleSheet.create({
   }
 });
 
-class Button extends React.Component {
+class IconButton extends React.Component {
   constructor() {
     super();
     extendObservable(this, { isDisabled: false });
@@ -33,21 +48,23 @@ class Button extends React.Component {
   };
 
   render() {
-    const { onPress, icon, style, color, ...rest } = this.props;
+    const { onPress, icon, style, color, size, ...rest } = this.props;
+
+    const Icon = icon;
+    const iconSize = getIconSize(size);
 
     return(
-      <View>
-        <Btn
-          icon={icon}
-          onPress={async (e) => this.onPress(e, onPress)}
-          style={[style, defaultStyle.iconButton]}
-          disabled={this.isDisabled}
-          color={color}
-          {...rest}
-        />
-      </View>
+      <Pressable onPress={async (e) => this.onPress(e, onPress)} disabled={this.isDisabled}>
+        <View style={[style, defaultStyle.iconButton]} {...rest}>
+          <Icon color={color} size={iconSize}/>
+        </View>
+      </Pressable>
     );
   }
 }
 
-export default observer(Button);
+IconButton.propTypes = {
+  size: PropTypes.oneOf(['xs', 'sm', 'md', 'lg'])
+};
+
+export default observer(IconButton);
