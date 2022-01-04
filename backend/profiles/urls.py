bab139e6512e0687578fb1c02e98c11b1576dd7d
alexbmoreira <alexbmoreira@gmail.com>
2022-01-03 23:41:18 -0500

    Generic watchlist view

--- backend/profiles/urls.py
+++ backend/profiles/urls.py
@@ -6,17 +6,6 @@ from .views import CurrentUserView, UserView, WatchlistView
 router = DefaultRouter()
 
 current_user_view = CurrentUserView.as_view()
-watchlist_view = WatchlistView.as_view()
-# user_search_api = UserView.as_view({'get':'list'})
-# user_detail_api = UserView.as_view({'get':'retrieve'})
-# friend_requests_api = FriendRequestAPIView.as_view()
-# friendships_api = FriendshipAPIView.as_view()
-# watchlist_api = WatchlistAPIView.as_view()
-# likes_api = MatchlistLikeAPIView.as_view()
-# dislikes_api = MatchlistDislikeAPIView.as_view()
-# matches_api = MatchlistMatchAPIView.as_view()
-# joint_watchlist_api = JointWatchlistAPIView.as_view()
-# movie_details_api = MovieDetailsForUserView.as_view()
 
 # urlpatterns = [
 #     path("user/requests/", friend_requests_api, name="friend_requests"),
@@ -25,16 +14,13 @@ watchlist_view = WatchlistView.as_view()
 #     path("user/matches/", matches_api, name="matches"),
 #     path("user/watchlist/", watchlist_api, name="watchlist"),
 #     path("user/joint-watchlist/", joint_watchlist_api, name="joint_watchlist"),
-#     path("user/", current_user_view, name="current_user"),
-#     path("users/", user_search_api, name="users"),
-#     path("users/<int:user_id>/", user_detail_api, name="user_detail"),
 #     path("users/<int:user_id>/friends/", friendships_api, name="friendships_all"),
 #     path("user/movie-details/<int:movie_id>/", movie_details_api, name="movie_details"),
 # ]
 
 router.register(r'users', UserView, basename='user')
+router.register(r'user/watchlist', WatchlistView, basename='user')
 
 urlpatterns = [
-    path("user/", current_user_view, name="current_user"),
-    path("user/watchlist/", watchlist_view, name="user_watchlist"),
+    path("user/", current_user_view, name="current_user")
 ] + router.urls
