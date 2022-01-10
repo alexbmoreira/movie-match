4d20e376f4d200c4834865fba03421414e7155c2
Alex Moreira <alexbmoreira@gmail.com>
2022-01-10 16:03:10 -0500

    GET Joint watchlists (#211)
    * GET Joint watchlists
    
    * tests
    
    * lint

--- backend/profiles/urls.py
+++ backend/profiles/urls.py
@@ -2,14 +2,16 @@ from django.urls import re_path
 from rest_framework.routers import DefaultRouter
 
 from .views import (CurrentUserView, FriendRequestView, FriendshipView,
-                    MatchlistDislikeView, MatchlistLikeView, MatchlistView,
-                    MovieDetailsForUserView, UserView, WatchlistView)
+                    JointWatchlistView, MatchlistDislikeView,
+                    MatchlistLikeView, MatchlistView, MovieDetailsForUserView,
+                    UserView, WatchlistView)
 
 router = DefaultRouter()
 
 current_user_view = CurrentUserView.as_view()
 movie_details_for_user_view = MovieDetailsForUserView.as_view()
 matchlist_view = MatchlistView.as_view()
+joint_watchlist_view = JointWatchlistView.as_view()
 
 # urlpatterns = [
 #     path('user/requests/', friend_requests_api, name='friend_requests'),
@@ -30,5 +32,6 @@ router.register(r'matchlists/(?P<user_id>\w+)/dislikes', MatchlistDislikeView, b
 urlpatterns = router.urls + [
     re_path(r'user/', current_user_view, name='current-user'),
     re_path(r'user/movie-details/(?P<movie_id>\w+)/', movie_details_for_user_view, name='movie-details'),
-    re_path(r'matchlists/(?P<user_id>\w+)/', matchlist_view, name='matchlist')
+    re_path(r'matchlists/(?P<user_id>\w+)/', matchlist_view, name='matchlist'),
+    re_path(r'joint-watchlists/(?P<user_id>\w+)/', joint_watchlist_view, name='joint-watchlist')
 ]
