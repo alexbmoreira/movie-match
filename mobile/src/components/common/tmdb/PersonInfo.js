5ee6542fe14f6434d13b7472b53c0d81f0c3f9db
alexbmoreira <alexbmoreira@gmail.com>
2022-01-10 21:17:48 -0500

    person info

--- mobile/src/components/common/tmdb/PersonInfo.js
+++ mobile/src/components/common/tmdb/PersonInfo.js
@@ -1,17 +1,23 @@
 import _ from 'lodash';
 import React from 'react';
-import { View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { Text } from '../typography';
+
+const style = StyleSheet.create({
+  department: {
+    marginBottom: 5
+  }
+});
 
 const PersonInfo = ({ item }) => {
   const { known_for_department, known_for } = item;
   return (
     <View>
-      <Text soft>{known_for_department}</Text>
+      <Text soft style={style.department}>{known_for_department}</Text>
       <View>
-        <Text>Known for:</Text>
+        {!_.isEmpty(known_for) && <Text>Known for:</Text>}
         {_.map(known_for.slice(0, 1), (movie) => (
-          <Text key={movie.id}>{`- ${movie.title || movie.name}`}</Text>
+          <Text bold key={movie.id}>{`${movie.title || movie.name}`}</Text>
         ))}
       </View>
     </View>
