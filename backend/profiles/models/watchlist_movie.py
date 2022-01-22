c2dd811a6698e2cd86a590db1cf04385b513e108
alexbmoreira <alexbmoreira@gmail.com>
2022-01-21 22:08:40 -0500

    add dates to models

--- backend/profiles/models/watchlist_movie.py
+++ backend/profiles/models/watchlist_movie.py
@@ -7,9 +7,11 @@ class WatchlistMovie(models.Model):
 
     user = models.ForeignKey(User, related_name='watchlist', on_delete=models.CASCADE)
     movie = models.IntegerField(blank=True, default=list)
+    added_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         unique_together = ['user', 'movie']
+        ordering = ['-added_at']
 
     def __str__(self):
         return f"({self.id}) {self.user.username} wants to watch {self.movie}"
