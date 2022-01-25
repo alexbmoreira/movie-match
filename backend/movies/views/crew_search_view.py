54e83ed7517ac32ff5441b808231a88fb836bba6
alexbmoreira <alexbmoreira@gmail.com>
2022-01-24 23:24:57 -0500

    Refactor tmdb views
    Ugly as fuck

--- backend/movies/views/crew_search_view.py
+++ backend/movies/views/crew_search_view.py
@@ -1,10 +1,11 @@
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from .tmdb_api_view import TmdbAPIView
+from .tmdb_api_view import TmdbAPIView, TmdbListAPIView
 
 
-class CrewSearchAPIView(TmdbAPIView):
+class CrewSearchAPIView(APIView):
 
     def get(self, request):
         search = request.GET.get('search', '')
@@ -13,19 +14,19 @@ class CrewSearchAPIView(TmdbAPIView):
         if search == '':
             return Response(data={'error': 'search param required'}, status=status.HTTP_400_BAD_REQUEST)
 
-        cached = self.is_cached(request, 'crew_search', search=search, page=page)
+        cached = TmdbListAPIView.is_cached(request, 'crew_search', search=search, page=page)
 
         if not cached:
-            crew = self.make_request('search/person', query=search, page=page)
+            crew = TmdbAPIView.make_request('search/person', query=search, page=page)
             crew['results'] = [actor for actor in crew['results'] if actor['known_for_department'] != "Acting"]
 
             for member in crew['results']:
                 if member['profile_path']:
-                    member['profile_link_sm'] = self.get_image('w154', member['profile_path'])
-                    member['profile_link_md'] = self.get_image('w500', member['profile_path'])
-                    member['profile_link_og'] = self.get_image('original', member['profile_path'])
+                    member['profile_link_sm'] = TmdbAPIView.get_image('w154', member['profile_path'])
+                    member['profile_link_md'] = TmdbAPIView.get_image('w500', member['profile_path'])
+                    member['profile_link_og'] = TmdbAPIView.get_image('original', member['profile_path'])
                 member['type'] = 'crew'
 
-            self.set_cache(request, 'crew_search', crew, search=search, page=page)
+            TmdbAPIView.set_cache(request, 'crew_search', crew, search=search, page=page)
 
         return Response(request.session['crew_search'])
--- backend/movies/views/tmdb_api_view.py
+++ backend/movies/views/tmdb_api_view.py
@@ -7,19 +7,14 @@ from rest_framework.views import APIView
 api_key = settings.TMDB_API
 
 
-class TmdbAPIView(APIView):
+class TmdbAPIView:
 
-    def make_request(self, endpoint, **kwargs):
-        params = ''.join([f"&{key}={value}" for key, value in kwargs.items()])
-
-        query = f"https://api.themoviedb.org/3/{endpoint}?api_key={api_key}{params}"
-        response = requests.get(query)
-        return response.json()
-
-    def get_image(self, size, path):
+    @staticmethod
+    def get_image(size, path):
         return f"https://image.tmdb.org/t/p/{size}{path}"
 
-    def is_cached(self, request, action, **kwargs):
+    @staticmethod
+    def is_cached(request, action, **kwargs):
         if action not in request.session or sys.getsizeof(request.session[action]) == 0:
             return False
 
@@ -29,8 +24,49 @@ class TmdbAPIView(APIView):
 
         return True
 
-    def set_cache(self, request, action, action_value, **kwargs):
+    @staticmethod
+    def set_cache(request, action, action_value, **kwargs):
         request.session[action] = action_value
 
         for key, value in kwargs.items():
             request.session[action][key] = value
+
+
+class TmdbListAPIView:
+
+    @staticmethod
+    def make_request(endpoint, **kwargs):
+        params = ''.join([f"&{key}={value}" for key, value in kwargs.items()])
+
+        query = f"https://api.themoviedb.org/3/{endpoint}?api_key={api_key}{params}"
+        response = requests.get(query)
+
+        results = response.json()['results']
+        total_results = response.json()['total_results']
+        total_pages = response.json()['total_pages']
+
+        return {
+            'count': total_results,
+            'next': True if len(results) < total_results and kwargs.get('page', 0) < total_pages else False,
+            'prev': True if kwargs.get('page', 0) > 1 else False,
+            'results': results
+        }
+
+    @staticmethod
+    def set_cache(request, action, action_value, **kwargs):
+        request.session[action] = {}
+        request.session[action]['results'] = action_value
+
+        for key, value in kwargs.items():
+            request.session[action][key] = value
+
+
+class TmdbRetrieveAPIView:
+
+    @staticmethod
+    def make_request(endpoint, **kwargs):
+        params = ''.join([f"&{key}={value}" for key, value in kwargs.items()])
+
+        query = f"https://api.themoviedb.org/3/{endpoint}?api_key={api_key}{params}"
+        response = requests.get(query)
+        return response.json()
