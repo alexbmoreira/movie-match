cd2e6a19d3f91ff1ee43e0fcc8d3c7498f84cd31
alexbmoreira <alexbmoreira@gmail.com>
2022-04-15 23:21:10 -0400

    remove comments

--- mobile/src/shared/url.js
+++ mobile/src/shared/url.js
@@ -23,18 +23,4 @@ class Url {
   }
 }
 
-// Url.prototype.toString = () => {
-//   let url = this.endpoint;
-
-//   if (!_.isEmpty(this.params)) {
-//     let numParams = 0;
-//     _.forEach(this.params, (value, key) => {
-//       url += `${numParams > 0 ? '&' : '?'}${key}=${value}`;
-//       numParams++;
-//     });
-//   }
-
-//   return url;
-// };
-
 export default Url;
