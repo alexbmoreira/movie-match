091ab93ad7e6f7a136a8d4fc16a26c3eac235cf1
Alex Moreira <alexbmoreira@gmail.com>
2022-05-26 23:43:00 -0400

    wip

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,9 +1,6 @@
 class ApplicationController < ActionController::API
   include Pundit
 
-  def newThing
-  end
-
   after_action :verify_authorized
   after_action :verify_policy_scoped, only: :index
 end
