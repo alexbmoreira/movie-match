90ce44c50175acf9400f8832206a8213ace89700
Alex Moreira <alexbmoreira@gmail.com>
2022-06-15 18:37:43 -0400

    pundit

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -9,21 +9,26 @@ class ApplicationController < ActionController::API
   include Sorcery::Controller
   include ActionController::Cookies
   include ActionController::RequestForgeryProtection
-  include SessionFix
 
   respond_to :json
 
-  before_action :require_login, except: [:not_found]
-  after_action :verify_authorized, except: [:not_found]
+  before_action :require_login, except: [:not_found, :not_authorized]
+  after_action :verify_authorized, except: [:not_found, :not_authorized]
   around_action :set_current_user
   before_action :force_json
 
-  def force_json
-    request.format = :json
+  rescue_from Pundit::NotAuthorizedError, with: :not_authorized
+
+  def not_authorized
+    head(:forbidden)
   end
 
   def not_found
-    render plain: 'Not found.', status: :not_found
+    head(:not_found)
+  end
+
+  def force_json
+    request.format = :json
   end
 
   def set_current_user(&block) # rubocop:disable Naming/AccessorMethodName
