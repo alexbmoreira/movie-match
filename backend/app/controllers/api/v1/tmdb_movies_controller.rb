b6b0633abc0a42d0ae1bca60e441e196397e9403
Alex Moreira <alexbmoreira@gmail.com>
2022-06-17 18:01:09 -0400

    Enforce Pundit

--- backend/app/controllers/api/v1/tmdb_movies_controller.rb
+++ backend/app/controllers/api/v1/tmdb_movies_controller.rb
@@ -1,8 +1,6 @@
 module Api
   module V1
     class TmdbMoviesController < ApplicationController
-      skip_after_action :verify_authorized, only: [:show]
-
       def show
         respond_with TmdbDetails::Movie.run(id: params[:id]),
           serializer: TmdbMovieDetailSerializer, include: [:cast_members, :crew_members]
