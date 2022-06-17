14eec2bb47d8e405dd898c3de047150526602e38
Alex Moreira <alexbmoreira@gmail.com>
2022-06-16 21:53:44 -0400

    Tmdb model specs

---
+++ backend/spec/models/movie_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+describe TmdbMovie do
+  subject { described_class.new(params) }
+
+  context 'when creating for details page' do
+    let(:params) { JSON.parse(File.read('spec/fixtures/tmdb/movie_details.json')) }
+
+    it do
+      is_expected.to have_attributes(
+        id: params['id'],
+        title: params['title'],
+        overview: params['overview'],
+        release_date: params['release_date'],
+        poster_path: params['poster_path'],
+        cast_members: include(a_kind_of(TmdbPerson)),
+        crew_members: include(a_kind_of(TmdbPerson))
+      )
+    end
+  end
+
+  context 'when creating for search results' do
+    let(:params) { JSON.parse(File.read('spec/fixtures/tmdb/movie_search.json')) }
+
+    it do
+      is_expected.to have_attributes(
+        id: params['id'],
+        title: params['title'],
+        release_date: params['release_date'],
+        poster_path: params['poster_path']
+      )
+    end
+  end
+end
