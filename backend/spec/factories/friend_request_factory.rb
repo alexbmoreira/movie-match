d0d0aabcef776a479640b2d53e8a467b0db9e27c
Alex Moreira <alexbmoreira@gmail.com>
2022-06-18 14:04:19 -0400

    wip

--- backend/spec/factories/friend_request_factory.rb
+++ backend/spec/factories/friend_request_factory.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :friend_request do
-    creator { create(:user, username: Faker::Games::Zelda.name.gsub(/[-\s']/, '_')) }
+    creator { create(:user, username: Faker::Cannabis.strain.gsub(/[-\s'#]/, '_')) }
     receiver { create(:user, username: Faker::Games::Minecraft.mob.gsub(/[-\s']/, '_')) }
   end
 end
--- backend/spec/factories/friendship_factory.rb
+++ backend/spec/factories/friendship_factory.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :friendship do
-    user { create(:user, username: Faker::Games::Zelda.name.gsub(/[-\s']/, '_')) }
+    user { create(:user, username: Faker::Cannabis.strain.gsub(/[-\s'#]/, '_')) }
     friend { create(:user, username: Faker::Games::Minecraft.mob.gsub(/[-\s']/, '_')) }
   end
 end
