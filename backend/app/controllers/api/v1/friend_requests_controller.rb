1dff44d6e52443d028fe4380c382ca46126a4deb
Alex Moreira <alexbmoreira@gmail.com>
2022-06-18 00:45:06 -0400

    Friend request deletion (#266)

--- backend/app/controllers/api/v1/friend_requests_controller.rb
+++ backend/app/controllers/api/v1/friend_requests_controller.rb
@@ -15,14 +15,24 @@ module Api
 
       def create
         respond_with FriendRequests::Create.run(receiver: find_receiver!),
-          each_serializer: versioned_class(FriendRequestSerializer),
+          serializer: versioned_class(FriendRequestSerializer),
+          include: [:creator, :receiver]
+      end
+
+      def destroy
+        respond_with FriendRequests::Destroy.run(friend_request: find_friend_request!),
+          serializer: versioned_class(FriendRequestSerializer),
           include: [:creator, :receiver]
       end
 
       private
 
+      def find_friend_request!
+        FriendRequests::Find.run!(id: params[:id])
+      end
+
       def find_receiver!
-        User.find(friend_request_params[:receiver_id])
+        Users::Find.run!(id: friend_request_params[:receiver_id])
       end
 
       def friend_request_params
---
+++ backend/spec/interactions/friend_requests/destroy_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe FriendRequests::Destroy do
+  wrap_user_context { user }
+
+  let(:user) { create(:user) }
+  let!(:friend_request) { create(:friend_request, creator: user) }
+  let(:params) {
+    {
+      friend_request: friend_request
+    }
+  }
+
+  subject { described_class.run!(params) }
+
+  it { expect { subject }.to change { FriendRequest.count }.by(-1) }
+
+  context 'when the user is the receiver' do
+    let(:friend_request) { create(:friend_request, receiver: user) }
+
+    it { expect { subject }.to change { FriendRequest.count }.by(-1) }
+  end
+
+  context 'when the request does not belong to the user' do
+    let(:friend_request) { create(:friend_request) }
+
+    it { expect { subject }.to raise_error Pundit::NotAuthorizedError }
+  end
+end
