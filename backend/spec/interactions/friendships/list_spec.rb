13ae7ec980837bdbd891d6ef3257c48cc7d8d6b0
Alex Moreira <alexbmoreira@gmail.com>
2022-06-23 21:55:43 -0400

    wip

--- backend/spec/interactions/friendships/list_spec.rb
+++ backend/spec/interactions/friendships/list_spec.rb
@@ -7,12 +7,10 @@ describe Friendships::List do
   let(:friend) { create(:user, username: 'sardine') }
   let(:other_user) { create(:user, username: 'eagle') }
   let!(:friendship1) { create(:friendship, user: user, friend: other_user) }
-  let!(:friendship2) { create(:friendship, friend: user, user: other_user) }
-  let!(:friendship3) { create(:friendship, user: user, friend: other_user) }
-  let!(:friendship4) { create(:friendship) }
+  let!(:friendship2) { create(:friendship) }
 
   subject { described_class.run! }
 
-  it { is_expected.to have(3).friendships }
-  it { expect(subject).to_not include(friendship4) }
+  it { is_expected.to have(1).friendships }
+  it { expect(subject).to_not include(friendship2) }
 end
