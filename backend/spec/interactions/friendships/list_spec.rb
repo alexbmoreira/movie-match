9a1a301ee2bef643b704e8b108e3a4c612239844
Alex Moreira <alexbmoreira@gmail.com>
2022-06-23 21:57:45 -0400

    Fix flakey test (#279)
    * Fix flakey test
    
    * wip

--- backend/spec/interactions/friendships/list_spec.rb
+++ backend/spec/interactions/friendships/list_spec.rb
@@ -5,13 +5,12 @@ describe Friendships::List do
 
   let(:user) { create(:user, username: 'hippopotamus') }
   let(:friend) { create(:user, username: 'sardine') }
-  let!(:friendship1) { create(:friendship, user: user) }
-  let!(:friendship2) { create(:friendship, friend: user) }
-  let!(:friendship3) { create(:friendship, user: user) }
-  let!(:friendship4) { create(:friendship) }
+  let(:other_user) { create(:user, username: 'eagle') }
+  let!(:friendship1) { create(:friendship, user: user, friend: other_user) }
+  let!(:friendship2) { create(:friendship) }
 
   subject { described_class.run! }
 
-  it { is_expected.to have(3).friendships }
-  it { expect(subject).to_not include(friendship4) }
+  it { is_expected.to have(1).friendships }
+  it { expect(subject).to_not include(friendship2) }
 end
--- backend/spec/interactions/watchlist_movies/list_spec.rb
+++ backend/spec/interactions/watchlist_movies/list_spec.rb
@@ -5,11 +5,10 @@ describe WatchlistMovies::List do
 
   let(:user) { create(:user) }
   let!(:watchlist_movie1) { create(:watchlist_movie, user: user) }
-  let!(:watchlist_movie2) { create(:watchlist_movie, user: user) }
-  let!(:watchlist_movie3) { create(:watchlist_movie) }
+  let!(:watchlist_movie2) { create(:watchlist_movie) }
 
   subject { described_class.run! }
 
-  it { is_expected.to have(2).watchlist_movies }
-  it { expect(subject).to_not include(watchlist_movie3) }
+  it { is_expected.to have(1).watchlist_movies }
+  it { expect(subject).to_not include(watchlist_movie2) }
 end
