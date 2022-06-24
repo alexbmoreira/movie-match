9910b2ea67a16714e732740d47c47d30eafd7e82
Alex Moreira <alexbmoreira@gmail.com>
2022-06-23 21:34:21 -0400

    Matchlist action create-destroy

---
+++ backend/spec/interactions/matchlist_actions/create_like_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe MatchlistActions::CreateLike do
+  wrap_user_context { user }
+
+  let(:user) { create(:user) }
+  let(:friend) { create(:user, username: 'sardine') }
+  let(:params) {
+    {
+      user: user,
+      friend: friend,
+      movie_id: '4995'
+    }
+  }
+
+  subject { described_class.run!(params) }
+
+  it { expect { subject }.to change { MatchlistActions::MatchlistLike.count }.by(1) }
+end
---
+++ backend/spec/interactions/matchlist_actions/destroy_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+describe MatchlistActions::Destroy do
+  wrap_user_context { user }
+
+  let(:user) { create(:user) }
+  let(:friend) { create(:user, username: 'sardine') }
+  let!(:matchlist_action) { create(:matchlist_like, user: user, friend: friend) }
+  let(:params) {
+    {
+      matchlist_action: matchlist_action
+    }
+  }
+
+  subject { described_class.run!(params) }
+
+  it { expect { subject }.to change { MatchlistActions::MatchlistLike.count }.by(-1) }
+
+  context 'when destroying a dislike' do
+    let!(:matchlist_action) { create(:matchlist_dislike, user: user, friend: friend) }
+
+    it { expect { subject }.to change { MatchlistActions::MatchlistDislike.count }.by(-1) }
+  end
+
+  context "when the movie does not belong to the user's watchlist" do
+    let(:matchlist_action) { create(:matchlist_like) }
+
+    it { expect { subject }.to raise_error Pundit::NotAuthorizedError }
+  end
+end
