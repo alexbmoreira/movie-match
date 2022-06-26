245495f21d0ee2e7a6aa2564f00e28c2d3e842e9
Alex Moreira <alexbmoreira@gmail.com>
2022-06-25 23:04:54 -0400

    ugh

--- mobile/src/components/navigation/MainTabs.js
+++ mobile/src/components/navigation/MainTabs.js
@@ -1,9 +1,10 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Icon } from 'components/common';
+import { View } from 'react-native';
+// import { Icon } from 'components/common';
 import React from 'react';
 import { theme } from 'shared';
-import { ProfileStack, SearchStack } from './stack_navigators';
-import MatchStack from './stack_navigators/MatchStack';
+// import { ProfileStack, SearchStack } from './stack_navigators';
+// import MatchStack from './stack_navigators/MatchStack';
 
 const Tab = createBottomTabNavigator();
 
@@ -18,38 +19,42 @@ const screenOptions = {
   tabBarShowLabel: false
 };
 
-const MainTabs = ({ route }) => {
-  return (
-    <Tab.Navigator screenOptions={screenOptions}>
-      <Tab.Screen
-        name='MatchStack'
-        component={MatchStack}
-        options={{
-          title: 'Match',
-          tabBarIcon: ({ size, color }) => <Icon name='match-cards' color={color} size={size}/>
-        }}
-        initialParams={route.params}
-      />
-      <Tab.Screen
-        name='SearchStack'
-        component={SearchStack}
-        options={{
-          title: 'Search',
-          tabBarIcon: ({ size, color }) => <Icon name='search' color={color} size={size}/>
-        }}
-        initialParams={route.params}
-      />
-      <Tab.Screen
-        name='ProfileStack'
-        component={ProfileStack}
-        options={{
-          title: 'Profile',
-          tabBarIcon: ({ size, color }) => <Icon name='user' color={color} size={size}/>
-        }}
-        initialParams={route.params}
-      />
-    </Tab.Navigator>
-  );
-};
+// const MainTabs = ({ route }) => {
+//   return (
+//     <Tab.Navigator screenOptions={screenOptions}>
+//       <Tab.Screen
+//         name='MatchStack'
+//         component={MatchStack}
+//         options={{
+//           title: 'Match',
+//           tabBarIcon: ({ size, color }) => <Icon name='match-cards' color={color} size={size}/>
+//         }}
+//         initialParams={route.params}
+//       />
+//       <Tab.Screen
+//         name='SearchStack'
+//         component={SearchStack}
+//         options={{
+//           title: 'Search',
+//           tabBarIcon: ({ size, color }) => <Icon name='search' color={color} size={size}/>
+//         }}
+//         initialParams={route.params}
+//       />
+//       <Tab.Screen
+//         name='ProfileStack'
+//         component={ProfileStack}
+//         options={{
+//           title: 'Profile',
+//           tabBarIcon: ({ size, color }) => <Icon name='user' color={color} size={size}/>
+//         }}
+//         initialParams={route.params}
+//       />
+//     </Tab.Navigator>
+//   );
+// };
+
+const MainTabs = () => (
+  <View/>
+);
 
 export default MainTabs;
