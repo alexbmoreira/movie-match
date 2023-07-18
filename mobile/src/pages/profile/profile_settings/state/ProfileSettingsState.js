79427f062b8ac2f7f31c25773bbb8d8d91e676e9
Alex Moreira <alexbmoreira@gmail.com>
2023-07-18 00:05:27 -0400

    logout

--- mobile/src/pages/profile/profile_settings/state/ProfileSettingsState.js
+++ mobile/src/pages/profile/profile_settings/state/ProfileSettingsState.js
@@ -2,6 +2,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { postRequest } from 'api';
 import { action, makeObservable, observable } from 'mobx';
 import { endpoints } from 'shared';
+import { authStore } from 'shared/stores';
 import { navigate } from 'shared/RootNavigation';
 
 class ProfileSettingsState {
@@ -18,8 +19,9 @@ class ProfileSettingsState {
     const data = await postRequest(endpoints.AUTH.LOGOUT);
 
     if(!data.errors) {
-      await AsyncStorage.removeItem('set-cookie');
+      await AsyncStorage.removeItem('token');
       await AsyncStorage.removeItem('user');
+      authStore.logout();
       navigate('Login');
     } else {
       this.errors = data.errors;
